1078fb9ef67bffbe622d6dd85ed384a9af77fcde
MuhammadHammad002 <hammadfayyazpolani@gmail.com>
2024-03-01 16:55:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,206 @@
+// Chap 4:VARIABLE NAMES: LEGAL & ILLEGAL
+
+// Q1
+// var a ="Hello World ";
+// var b = "hammad";
+// var c = "fayyaz";
+// alert(a+b+c);
+
+// Q2
+// var Legal names dollar,index,logo,heading,fullName ;
+// var Ilegal names prompt,var,1heading,@code,-myname;
+
+// Q3
+// var a ="Rules for naming JS variables";
+// document.write("<h1>" + a + "</h1>" +"<br/>");
+// var b ="Variable names can only contain numbers,$,_ .For example $my_1stVariable";
+// document.write(b +"<br/>");
+// var c = "Variables must begin with a letter,$,_ . For example $name, _name or name";
+// document.write(c +"<br/>");
+// var d = "Variable names are case sensitive";
+// document.write(d + "<br/>");
+// var e = "Variable names should not be JS keywords";
+// document.write(e + "<br/>");
+
+// Chap 5 :MATH EXPRESSIONS
+
+// Q1
+// var sum = "Sum of 3 and 5 is " ;
+// var a = 5;
+// var b = 3;
+// var res= a+b;
+// document.write(sum + res);
+
+// Q2
+// var sum = "Sum of 3 minus 5 is ";
+// var a = 5;
+// var b = 3;
+// var res= a-b;
+// document.write(sum + res);
+// var sum = "Sum of 3 divide by 5 is ";
+// var a = 5;
+// var b = 3;
+// var res= a/b;
+// document.write(sum + res);
+// var sum = "Sum of 3 multiply 5 is ";
+// var a = 5;
+// var b = 3;
+// var res= a*b;
+// document.write(sum + res);
+// var sum = "Sum of 3 modulus by 5 is ";
+// var a = 5;
+// var b = 3;
+// var res= a%b;
+// document.write(sum + res);
+
+// Q3
+// var msg = 'Value after variable declaration is undefined';
+// document.write(msg  + "<br/>");
+// var msg2 ="Initial value: ";
+// var num = 5;
+// document.write( msg2+ num + "<br/>");
+// var msg3 = "Value after increment is: " ;
+// var num = ++num;
+// document.write( msg3+ num + "<br/>");
+// var msg4 = 'Value after addition is: ';
+// var num = num+7;
+// document.write( msg4+ num + "<br/>");
+// var msg5 = 'Value after decrement is: ';
+// var num = --num;
+// document.write( msg5+ num + "<br/>");
+// var msg5 = 'The remainder is: ';
+// var num = num/3;
+// document.write( msg5+ num + "<br/>" );
+
+// Q4
+// var store ="Total cost to buying 5 tickets to a movie is ";
+// var ticket = 600;
+// var res = ticket * 5;
+// var currency = "PKR";
+// document.write( store+ res + currency );
+
+// Q5
+// document.write("Table of 4" + "<br/>"+; 
+// "4*1=4"+ "<br/>"+;
+// "4*2=8"+ "<br/>"+;
+// "4*3=12"+ "<br/>"+;
+// "4*4=16"+ "<br/>"+;
+// "4*5=20"+ "<br/>"+;
+// "4*6=24"+ "<br/>"+;
+// "4*7=28"+ "<br/>"+;
+// "4*8=32"+ "<br/>"+;
+// "4*9=36"+ "<br/>"+;
+// "4*10=40"+ "<br/>"+;
+// "4*11=44"+ "<br/>"+;
+// "4*12=48"+ "<br/>";
+// )
+
+// Q6
+// var celsius = "25°C is 77°F";
+// var fahrenheit = "70°F is 21.11111111111111°C";
+// document.write( celsius+ "<br/>"  + fahrenheit );
+
+// Q7
+// var item1="Price of item 1 is 650";
+// var quantity1="Quantity of item 1 is 3";
+// var item2 ="Price of item 2 is 100";
+// var quantity2="Quantity of item 2 is 7";
+// var shipping = "Shipping Charges 100";
+// var tcost = "Total cost of your order is 2750";
+// document.write( item1 + "<br/>"+quantity1 + "<br/>"+  item2 + "<br/>" +quantity2 + "<br/>" + shipping + "<br/>" + "<br/>" + tcost  );
+
+// Q8
+// var marksheet = "Mark Sheet";
+// var totalMarks ="Total marks: 980";
+// var marksObtained="Marks obtained: 840";
+// var percentage="Percentage: 82.0408163265306%";
+// document.write("<h1>" + marksheet + "</h1>" + "<br/>" +totalMarks + "<br/>" + marksObtained + "<br/>" + percentage  );
+
+// Q9
+// var pkr = "Currency in PKR";
+// var totalcurrency =" Total Currency in PKR:";
+// var num = 104.80*10+28*25;
+// document.write("<h1>" + pkr + "</h1>" + "<br/>" + totalcurrency +num );
+
+// Q10
+// var num = 2+18*2/2;
+// document.write(num);
+
+// Q11
+//  var cal= "Age Calculator";
+//  var cy = "Current Year: 2016";
+//  var by = "Birth Year: 1992";
+//  var age = "Your Age is: 24";
+//  document.write("<h1>" + cal + "</h1>" + "<br/>" + cy + "<br/>" + by + "<br/>" + age + "<br/>" );
+
+// Q12
+// var geo= "The Geometrizer";
+// var rad = "Radius of a circle: 20";
+// var circum ="The circumference is: 125.67999999999999";
+// var area = "The area is: 1256.8";
+// document.write("<h1>" + geo + "</h1>" + "<br/>" + rad + "<br/>" + circum + "<br/>"+ area );
+
+// Q13
+//   var cal= "The Lifetime Supply Calculator";
+//   var favSnack =" Favourite Snack: chocolate chip"
+//   var age = "Current age: 15";
+//   var max = "Estimated Maximum Age: 65"
+//   var eat = "Amount of snacks per day: 3"
+//   var output = "You will need 150 chocolate chip to last you until the ripe old age of 65."
+//  document.write("<h1>" +cal  + "</h1>" + "<br/>" + favSnack + "<br/>" + age + "<br/>"  +max + "<br/>" + eat + "<br/>" + output);
+
+// Chap 6 Math expressions: Unfamiliar operators
+
+// Q1
+//  var result = "Result:";
+//  var msg = "The value of a is:";
+//  var num = 10;
+//  var dot ="..........................................";
+//  var msg1 = "The value of ++a is:11";
+//  var msg2 = "Now the value of a is:11";
+//  var msg3 = "The value of a++ is:11";
+//  var msg4 = "Now the value of a is:12";
+//  var msg3 = "The value of a++ is:11";
+//  var msg4 = "Now the value of a is:12";
+//  var msg5 = "The value of --a is:11";
+//  var msg6 = "Now the value of a is:11";
+//  var msg7 = "The value of a-- is:11";
+//  var msg8 = "Now the value of a is:10";
+//  document.write(result  + "<br/>" + msg  + num + "<br/>" + dot + "<br/>" + "<br/>" + msg1  + "<br/>" + msg2   +
+//  "<br/>" + "<br/>" + msg3  + "<br/>" + msg4 + "<br/>" + "<br/>" + msg5  + "<br/>" + msg6 +  "<br/>" + "<br/>" + msg7 + "<br/>" + msg8);
+
+// Q2
+// var a = 2;
+// var b = 1;
+// var res= --a - --b + ++b + b--;
+// var a1= " a = 1"
+// var b1 = " b = 2 <br/> res ="
+// document.write( a1 + "<br/>" +b1 + res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
